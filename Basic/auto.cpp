4cfddd96fb1f4f59043a66728ea764b9462341fb
z00593 <zhangao@henggetec.com>
2024-03-07 09:56:24 +0800

    add basic体系系列

---
+++ Basic/auto.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+int main()
+{
+    const int a_const = 10;
+    // a_const = 20;// 表达式必须是可修改的左值。 const提示编译器这个值不可修改，但它其实仍存储在变量区
+    auto b = a_const; // 推断不包含const
+    b = 1;
+   
+    std::cout << "b: " << b << std::endl;
+    std::cout << "a: " << a_const << std::endl;
+    
+    auto& a_const_ref = a_const;
+    //a_ref = 20; // 表达式必须是可修改的左值
+
+    int a_var = 10;
+
+    auto& a_var_ref = a_var;
+    a_var_ref = 5;
+
+    std::cout << "a_const_ref: " <<  a_const_ref << std::endl;
+    std::cout << "a_const: " << a_const << std::endl;
+
+    std::cout << "a_var_ref: " << a_var_ref << std::endl;
+    std::cout << "a_var: " << a_var << std::endl;
+    return 0;
+}
---
+++ Basic/const.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main()
+{
+    int test_int = 30;
+    /// const 在*左边，限制指针指向的内容
+    const int* p_left = new int(10);
+    p_left = &test_int; // 可以修改指针指向的地址
+    // *p = 20; // 不可修改指针指向的内容
+    // p_left = test_int;
+    /// 另一种写法
+    int const* p_left1 = new int(40);
+    // *p_left1 = 20; // 表达式必须是可修改的左值
+    p_left1 = &test_int;
+     
+    /// const 在*右边，限制指针的地址
+    int* const p_right = new int(20);
+    *p_right = 10;
+    // p_right = &test_int; // 表达式必须是可修改的左值
+
+    std::cout << "p_left: " << * p_left << std::endl;
+    std::cout << "p_left1: " << *p_left1 << std::endl;
+    std::cout << "p_right: " << *p_right << std::endl;
+    return 0;
+}
