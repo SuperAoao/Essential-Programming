80493f3252f070f2d98a061197ebb3e1dd937806
z00593 <zhangao@henggetec.com>
2024-03-14 13:29:31 +0800

    add: interview questions

---
+++ Interview/func.cpp
@@ -0,0 +1,13 @@
+﻿class Test
+{
+    Test(int i){}
+    Test(){}
+    void func(){}
+};
+
+int main()
+{
+    Test a(10);
+    a.func();
+    return 0;
+}
---
+++ Interview/sizeof.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+char* g_str = "Hello";
+
+int main()
+{
+    std::cout << "sizeof(g_str): " << sizeof(g_str) << std::endl;
+    std::cout << "sizeof(*g_str): " << sizeof(*g_str) << std::endl;
+    std::cout << "strlen(g_str): " << strlen(g_str) << std::endl;
+    return 0;
+}
