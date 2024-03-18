f8a8e2c0b394a6e5dc8ba6151e9ac037d888c894
AoZhang <3390531267@qq.com>
2024-03-18 20:18:50 +0800

    add part4_sharedptr.cpp

---
+++ Basic/part4_sharedptr.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+
+
+int main()
+{
+	std::shared_ptr<int> s1 = std::make_shared<int>(20);
+	std::cout << "s1: " << s1 << std::endl;
+	std::cout << s1.unique() << std::endl;
+	std::cout << s1.use_count() << std::endl;
+	return 0;
+}
