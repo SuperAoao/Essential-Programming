c6a3614cff1becb504528cc131c37e926a1d897a
AoZhang <3390531267@qq.com>
2024-03-19 21:09:30 +0800

    template class with move constructor

--- Basic/part5_template_class.cpp
+++ Basic/part5_template_class.cpp
@@ -1,7 +1,7 @@
 #include "part5_template_class.hpp"
 #include <vector>
 #include <type_traits>
-
+#include <iostream>
 int main()
 {
 	std::initializer_list<int> list = { 1,2,3,4,5 };
@@ -11,7 +11,17 @@ int main()
 	int i1 = 10;
 	int i2 = 20;
 	std::initializer_list<int*> list_pointer = { &i1, &i2 };
+	MyArray<int*> arr(list_pointer);
+	for (int i = 0; i < 2; i++)
+	{
+		std::cout << *arr[i] << std::endl;
+	}
 
+	MyArray<int*> arrMove(std::move(list_pointer));
+	for (int i = 0; i < 2; i++)
+	{
+		std::cout << *arrMove[i] << std::endl;
+	}
 
 	MyArray<int> a(5);
 	a.begin();
--- Basic/part5_template_class.hpp
+++ Basic/part5_template_class.hpp
@@ -2,6 +2,17 @@
 #include <initializer_list>
 #include <type_traits>
 template <typename T>
+struct getType
+{
+	using type = T;
+};
+template <typename T>
+struct getType<T*>
+{
+	using type = T;
+};
+
+template <typename T>
 class MyArray
 {
 	using iterator = T*;
@@ -17,14 +28,21 @@ public:
 		{
 			m_data = nullptr;
 		}
-	}
+	};
 	MyArray(const std::initializer_list<T>& list);
+	MyArray(std::initializer_list<T>&& list);
 	~MyArray();
+	virtual T& operator[](unsigned int i) const
+	{
+		return m_data[i];
+	};
 	iterator begin() const;
 	const_iterator cbegin() const;
 private:
 	T* m_data;
 };
+
+
 template <typename T>
 MyArray<T>::~MyArray()
 {
@@ -54,12 +72,56 @@ MyArray<T>::MyArray(const std::initializer_list<T>& list)
 		unsigned int count = 0;
 		m_data = new T[list.size()];
 		// ›Õ»°ºº ı£¨
-		if (std::is_pointer<T>)
+		if (std::is_pointer<T>::value)
 		{
-			for (const val&: list)
+			for (auto elem: list)
 			{
-				m_data[count++] = *val;
+				m_data[count++] = new typename getType<T>::type(*elem);
 			}
 		}
+		else
+		{
+			for (const auto& elem : list)
+			{
+				m_data[count++] = elem;
+			}
+		}
+	}
+	else
+	{
+		m_data = nullptr;
+	}
+}
+
+template <typename T>
+MyArray<T>::MyArray(std::initializer_list<T>&& list)
+{
+	if (list.size())
+	{
+		unsigned int count = 0;
+		m_data = new T[list.size()];
+	
+		if (std::is_pointer<T>::value)
+		{
+			for (auto elem : list)
+			{
+				m_data[count++] = elem;
+				elem = nullptr;
+			}
+		}
+		else
+		{
+			if (std::is_pointer<T>::value)
+			{
+				for (auto elem : list)
+				{
+					m_data[count++] = elem;
+				}
+			}
+		}
+	}
+	else
+	{
+		m_data = nullptr;
 	}
 }
