90612468166c7c997912f4211fd2a3fd78b14f20
AoZhang <superao@163.com>
2024-03-26 15:46:45 +0800

    add string

---
+++ Basic/part6_stl_string.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string s1("Hello");
+    s1 + " World";
+    std::string s2 = s1 + " World";
+    std::cout << s1 + " World" << std::endl;
+    std::cout << s1 << std::endl;
+    std::cout << s2 << std::endl;
+
+    return 0;
+}
