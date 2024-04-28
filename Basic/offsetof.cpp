9876f79e5dd4d00e82ea67426c15b9e04376f86a
AoZhang <superao@163.com>
2024-04-28 09:53:17 +0800

    add offsetof.cpp

---
+++ Basic/offsetof.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+struct Vertex
+{
+	float Position[3];
+	float Normal[3];
+	float TexCoords[2];
+};
+
+int main()
+{
+	Vertex v;
+	std::cout << "offsetof Posistion:" << offsetof(Vertex, Position) << std::endl;
+	std::cout << "offsetof Normal:" << offsetof(Vertex, Normal) << std::endl;
+	std::cout << "offsetof TexCoords:" << offsetof(Vertex, TexCoords) << std::endl;
+
+	std::cout << "test Normal offset: " << (unsigned int)(((Vertex*)0)->Normal) << std::endl;
+	return 0;
+}
